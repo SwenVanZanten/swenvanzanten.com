58e79dee9137d7e1cd8f04a36125f1b80c17388d
Swen van Zanten <info@hdsign.nl>
2020-11-28 12:01:50 +0100

    Add temporary music services links

--- pages/music/index.vue
+++ pages/music/index.vue
@@ -14,7 +14,44 @@
     </section>
     <section class="section">
       <div class="container">
-        Sorry, soon more info...
+        <div class="columns">
+          <div class="column">
+            My previously released music can be found on the following services:
+          </div>
+          <div class="column">
+            <b-message type="is-success">
+              <a href="https://open.spotify.com/artist/5R9Upj7DCtU8VBTPKnAx2i">
+                Spotify
+              </a>
+            </b-message>
+            <b-message type="is-danger">
+              <a
+                href="https://music.apple.com/us/artist/swen-van-zanten/1227256117"
+              >
+                Apple Music
+              </a>
+            </b-message>
+            <b-message>
+              <a
+                href="https://play.google.com/store/music/album/Swen_Van_Zanten_Earth?id=Beyfobjz5qmoe242ov3lvxf6yoy"
+              >
+                Google Play/YouTube
+              </a>
+            </b-message>
+            <b-message type="is-warning">
+              <a
+                href="https://www.amazon.com/gp/product/B075HPDKQT/?tag=distrokid06-20"
+              >
+                Amazon Music
+              </a>
+            </b-message>
+            <b-message type="is-primary">
+              <a href="https://www.deezer.com/nl/artist/12314014">
+                Deezer
+              </a>
+            </b-message>
+          </div>
+        </div>
       </div>
     </section>
   </div>
